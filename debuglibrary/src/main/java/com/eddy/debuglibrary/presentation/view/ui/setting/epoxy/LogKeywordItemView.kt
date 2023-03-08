42e8ddec57838f25a61c15dec36f2d29a3d31701
snaps-eddy <eddy.jang@snaps.com>
2023-03-08 10:57:30 +0900

    feat: 0.0.35 업데이트
    수정내역:
    1. 필터링 키워드 삭제 기능 추가.
    2. 버그 수정.
    
    너무 지저분해짐. 바뻐서.. 리팩토링 해야겠다

--- debuglibrary/src/main/java/com/eddy/debuglibrary/presentation/view/ui/setting/epoxy/LogKeywordItemView.kt
+++ debuglibrary/src/main/java/com/eddy/debuglibrary/presentation/view/ui/setting/epoxy/LogKeywordItemView.kt
@@ -1,8 +1,10 @@
 package com.eddy.debuglibrary.presentation.view.ui.setting.epoxy
 
 import android.view.ViewParent
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.isVisible
 import com.airbnb.epoxy.EpoxyAttribute
 import com.airbnb.epoxy.EpoxyModelClass
 import com.airbnb.epoxy.EpoxyModelWithHolder
@@ -16,17 +18,25 @@ internal abstract class LogKeywordItemView : EpoxyModelWithHolder<LogFilterKeywo
     @EpoxyAttribute
     lateinit var content: String
 
+    @EpoxyAttribute(EpoxyAttribute.Option.DoNotHash)
+    var onClickTrash: (() -> Unit)? = null
+
     override fun bind(holder: LogFilterKeywordItemViewHolder) {
         with(holder) {
+            trashView.isVisible = content != "normal"
             textView.apply {
                 text = content
+            }
+
+            trashView.setOnClickListener {
+                onClickTrash?.invoke()
             }
         }
     }
 }
 
-
 internal class LogFilterKeywordItemViewHolder(parent: ViewParent) : KotlinEpoxyHolder() {
     val root by bind<LinearLayout>(R.id.root)
     val textView by bind<TextView>(R.id.tv_content)
+    val trashView by bind<ImageView>(R.id.iv_trash_keyword)
 }

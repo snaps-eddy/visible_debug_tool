38c71a802216df5f800ccb90a34d890a0291e310
snaps-eddy <eddy.jang@snaps.com>
2023-03-13 10:36:22 +0900

    feat: 0.0.36 업데이트
    수정내역:
    1. 버그 수정.
    2. 테스트 코드 작성 추가 중.
    
    리팩토링 하면서 테스트 코드 작성해야함.

---
+++ debuglibrary/src/test/java/com/eddy/debuglibrary/util/di/MainCoroutineRule.kt
@@ -0,0 +1,26 @@
+package com.eddy.debuglibrary.util.di
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MainCoroutineRule(
+    val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
+    override fun starting(description: Description) {
+        super.starting(description)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        super.finished(description)
+        Dispatchers.resetMain()
+    }
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun MainCoroutineRule.newCoroutineScope(): TestScope = TestScope(testDispatcher)
